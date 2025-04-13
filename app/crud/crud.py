657a80b69ee70e1301276e3364081f6bb554a6f8
nafernandez302 <nahuelfernandezit@gmail.com>
2025-04-13 16:49:09 -0300

    new: user getter

--- app/crud/crud.py
+++ app/crud/crud.py
@@ -1,5 +1,6 @@
 """Module with crud operations"""
 
+from sqlalchemy import Integer
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserSchema
@@ -14,3 +15,11 @@ def create_user(db: Session, user: UserSchema):
     db.commit()
     db.refresh(_user)
     return _user
+
+def get_user_by_id(db: Session, user_id: Integer):
+    """Get user information by id."""
+    return db.query(User).filter(User.id == user_id)
+
+def get_users(db: Session, limit: int=100):
+    """Get all users."""
+    return db.query(User).limit(limit).all()
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,40 @@
-from app.crud import crud
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.user import UserSchema, UserResponse
-from fastapi import APIRouter
-from fastapi import Depends
-from sqlalchemy.orm import Session
-router = APIRouter()
+from app.crud import crud
+from typing import List, Union
 
+router = APIRouter()
 
 @router.post("/create")
 async def create_user(request: UserSchema, db: Session = Depends(get_db)):
-    crud.create_user(db, user=request)
-    print(request)
-    return UserResponse(status="Ok",
-                    code="200",
-                    message="User created successfully",result=request).dict(exclude_none=True)
+    """
+    Create a new user in the database.
+    """
+    user = crud.create_user(db, user=request)
+    return UserResponse(
+        status="Ok",
+        code="200",
+        message="User created successfully",
+        result=user
+    ).dict(exclude_none=True)
 
+
+@router.get("/", response_model=List[UserSchema])
+async def get_users(limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Retrieve a list of users.
+    """
+    return crud.get_users(db, limit=limit)
+
+
+@router.get("/{user_id}", response_model=Union[UserSchema, dict])
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    """
+    Retrieve a user by their ID.
+    """
+    user = crud.get_user_by_id(db, user_id=user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
